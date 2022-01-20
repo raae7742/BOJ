9bb32bb649141f8eba25e41ef04a496d73fc58c6
Hyeonae Jang <aeae_1@naver.com>
2022-01-20 16:14:26 +0900

    20220120

---
+++ Bronze/11091.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+#define MAX 51
+
+using namespace std;
+
+void init() {
+	cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(0);
+}
+
+int N;
+string input;
+bool alpha[26];
+
+int main() {
+	cin >> N;
+	cin.ignore();
+	while (N--) {
+		queue<char> q;
+		memset(alpha, false, 26);
+		getline(cin, input);
+		for (int i = 0; i < input.size(); i++) {
+			char c = input.at(i);
+			if (c >= 'a' && c <= 'z') alpha[c - 'a'] = true;
+			if (c >= 'A' && c <= 'Z') alpha[c - 'A'] = true;
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if (!alpha[i]) q.push('a' + i);
+		}
+
+		if (!q.size()) cout << "pangram" << "\n";
+		else {
+			cout << "missing ";
+			while (!q.empty()) {
+				cout << q.front();
+				q.pop();
+			}
+			cout << "\n";
+		}
+	}
+
+	return 0;
+}
