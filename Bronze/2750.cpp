cb73ff9176d804cf9f9dda3e75c869c7b95a808c
hyeonae jang <aeae_1@naver.com>
2022-10-09 18:46:51 +0900

    수 정렬하기

---
+++ Bronze/2750.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 1001
+
+using namespace std;
+
+int arr[MAX], n;
+
+void init() {
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(false);
+}
+
+
+int main() {
+    init();
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << '\n';
+}
