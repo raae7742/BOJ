e68eddc0fec89cab43abb62b91ddb8a845082dac
hyeonae jang <aeae_1@naver.com>
2023-01-16 12:39:16 +0900

    Create 27212.java

---
+++ Gold/27212.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    private long max(long a, long b, long c) {
+        return Math.max(a, Math.max(b, c));
+    }
+    
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int tmp = Integer.parseInt(st.nextToken());
+        
+        int[][] w = new int[tmp+1][tmp+1];
+        for (int i = 1; i <= tmp; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= tmp; j++)
+                w[i][j] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] arrA = new int[r+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= r; i++)
+            arrA[i] = Integer.parseInt(st.nextToken());
+        
+        int[] arrB = new int[c+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= c; i++)
+            arrB[i] = Integer.parseInt(st.nextToken());
+        
+        long[][] dp = new long[r+1][c+1];
+        for (int i = 1; i <= r; i++)
+            for (int j = 1; j <= c; j++)
+                dp[i][j] = max(dp[i-1][j-1] + w[arrA[i]][arrB[j]], dp[i-1][j], dp[i][j-1]);
+        
+        System.out.println(dp[r][c]);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
